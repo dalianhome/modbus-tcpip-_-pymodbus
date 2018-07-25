9423407e5e4cd8ee4db8d0b5a156771f98838521
CX <samlee20051985@gmail.com>
2018-07-25 15:20:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# chengxin 24/7/2018 testing pymodbus
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 from pymodbus.register_read_message import ReadHoldingRegistersResponse
 import logging
@@ -6,8 +7,9 @@ import sys
 import time
 import datetime
 
-# host = '192.168.0.10'
-host='localhost'
+# tcp slave ip addr
+host = '192.168.0.10'
+
 port = 502
 print("yo, amigo, this is local folder")
 client = ModbusClient(host, port)
@@ -15,20 +17,15 @@ connection = client.connect()
 socketOpen = client.is_socket_open()
 print(connection)
 print(socketOpen)
+#above code make sure connection is done
 
-# client = ModbusTcpClient('127.0.0.1')
-# client.write_coil(1, True)
-# result = client.read_coils(1,2)
-# print(result.bits[0])
-# client.close()
 i = True
 num1=0
 q=1
 print("Addr" + "    Data " + "           Logging Time")
+
 while i:
  request = client.read_holding_registers(0x64,2,unit=1)
-# response = client.execute(request)
-#  assert (request.function_code < 0x80)
 
  if num1 != request.registers[0]:
     ts = time.time()
@@ -39,9 +36,8 @@ while i:
     f.write('\n' + str(q) + ". "+str(request.registers[0]) + "            "+st)
     num1 = request.registers[0]
     q = q + 1
- # else:
- #    print("----------silent----------")
- # print(q)
+
+    
  time.sleep(1)
 
  if i == False:
